9fd335fb3328320e57ca3ae3815f829b822a55e0
Lincoln Garcez <lincoln.garcez@sefaz.am.gov.br>
2025-03-19 14:29:10 -0400

    Update

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,7 +1,10 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
@@ -14,10 +17,13 @@ public record DadosCadastroMedico(
         String email,
 
         @NotBlank
-
+        @Pattern(regexp = "\\d{4,6}")
         String crm,
 
+        @NotNull
         Especialidade especialidade,
 
+        @NotNull
+        @Valid
         DadosEndereco endereco) {
 }
