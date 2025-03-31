3c8d231eab71898b65ae1b96996ff588c3915522
Lincoln Garcez <lincoln.garcez@sefaz.am.gov.br>
2025-03-31 13:40:42 -0400

    Update

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,8 +2,6 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
