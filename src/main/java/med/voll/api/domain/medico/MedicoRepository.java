9d48d309a512565e32260ad59be48b7abb2cfe8c
Lincoln Garcez <lincoln.garcez@sefaz.am.gov.br>
2025-04-03 16:46:27 -0400

    Update

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -19,14 +19,17 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             m.ativo = true
             and
             m.especialidade = :especialidade
-            and m.id not in(
+            and
+            m.id not in(
                 select c.medico.id from Consulta c
                 where
                 c.data = :data
+        and
+                c.motivoCancelamento is null
             )
             order by rand()
             limit 1
-            """)
+        """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, @NotNull @Future LocalDateTime data);
 
     @Query("""
--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -27,7 +27,8 @@ public class SecurityConfigurations {
                 http.csrf(csrf -> csrf.disable())
                         .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                         .authorizeHttpRequests(req -> {
-                            req.requestMatchers("/login").permitAll();
+                            req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                            req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
                             req.anyRequest().authenticated();
                         })
                         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
